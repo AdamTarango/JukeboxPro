d8518fad168d54fc223c9f4c3fe1043ceb808a85
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-11-21 16:42:46 -0800

    add auth router

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,42 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 function createToken (id) {
-  return jwt.sign({ id }, JWT_SECRET, { expiresIn: '1d '})
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: '1d'})
 }
 
 const prisma = require("../prisma");
+
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.register(username, password);
+    const token = createToken(user.id);
+    res.status(201).json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.login(username, password);
+    const token = createToken(user.id);
+    res.json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
+function authenticate(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You must be logged in." });
+  }
+}
+
+module.exports = {
+  router,
+  authenticate,
+};
